87b73747bb93174584b6504ad0c74d614c705d4e
Thevic16 <20171408@ce.pucmm.edu.do>
2021-04-27 23:32:47 -0400

    main-form view is working

--- src/main/java/edu/pucmm/eict/Controllers/MainController.java
+++ src/main/java/edu/pucmm/eict/Controllers/MainController.java
@@ -3,7 +3,7 @@ package edu.pucmm.eict.Controllers;
 import edu.pucmm.eict.utils.BaseController;
 import io.javalin.Javalin;
 
-import static io.javalin.core.security.SecurityUtil.roles;
+
 
 public class MainController extends BaseController {
 
@@ -17,6 +17,16 @@ public class MainController extends BaseController {
         app.get("/", ctx -> {
             ctx.render("/public/templates/login.html");
         });
+
+        app.get("/create-form", ctx -> {
+            ctx.render("/public/templates/main-form.html");
+        });
+
+        app.get("/list-form", ctx -> {
+            ctx.render("/public/templates/list-form.html");
+        });
+
+
 
     }
 
